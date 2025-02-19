68e2e3fd1b5b13a4f821e671277eaca95f0359a5
Faiz Alam <faizalam2432005@gmail.com>
2025-02-19 18:17:18 +0530

    Hello

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import React from "react";
 function App() {
   return (
     <div>
-      <h1 class="text-3xl font-bold underline">Hello world!</h1>
+      <h1 class="text-3xl font-bold underline">By world!</h1>
     </div>
   );
 }
